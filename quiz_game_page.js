ffeaf543b03e5b8cddc5e2eedf2795b231880457
Divyanshi239 <75576256+Divyanshi239@users.noreply.github.com>
2021-01-27 21:19:01 +0530

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -4,20 +4,23 @@ player2_name = localStorage.getItem("player2_name");
 console.log(player1_name);
 console.log(player2_name);
 
+player1_score = 0;
+player2_score = 0;
+
 document.getElementById("player1_name").innerHTML = player1_name;
 document.getElementById("player2_name").innerHTML = player2_name;
 
 question_turn = "player1";
 answer_turn = "player2";
 
-function Send()
+function send()
 {
     number1 = document.getElementById("number1").value;
     number2 = document.getElementById("number2").value;
     actual_answer = parseInt(number1) * parseInt(number2);   
     question_number = "<h4>" + number1 +" X " + number2 + "</h4>";
-    input_box = "<br>Answer: <input type='text' id='input_check_box'>";
-    check_button = "<br> <br> <button class='btn btn-info' onclick='check()'>Check</button>";
+    input_box = "<br> Answer: <input type='text' class='form-control' id='input_check_box'>";
+    check_button = "<br> <br> <button class='btn btn-info' onclick='check()'> Check </button>";
     row = question_number + input_box + check_button;
     document.getElementById("output").innerHTML = row;
     document.getElementById("number1").value = "";
@@ -27,7 +30,8 @@ function Send()
 function check()
 {
     get_answer = document.getElementById("input_check_box").value;
-    if(get_anwer == actual_answer)
+
+    if(get_answer == actual_answer)
     {
         if(answer_turn == "player1")
         {
@@ -48,6 +52,6 @@ function check()
     else
     {
         question_turn = "player1";
-        document.getElementById("player_questiion").innerHTML = "Question Turn- " + player1_name;
+        document.getElementById("player_question").innerHTML = "Question Turn- " + player1_name;
     }
 }
